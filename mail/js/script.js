e20aecda4bb5d508db5ebb7af4bb17e5f5fe5f5d
moonymel <musumeci.me@gmail.com>
2023-11-24 10:05:22 +0100

    correzione in classe esercizio mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,15 +5,21 @@ let new_mail = prompt('Inserisci qui la tua mail');
 // CREO ARRAY PER LE MAIL CONVALIDATE
 let mails = ['pippo@gmail.com', 'ciccio@gmail.com', 'tizio@gmail.com'];
 
+let flag = false;
+
 // CREO CICLO DI TUTTE LE MAIL CONVALIDATE
-for (let i=0; i<=mails.length; i++) {
+for (let i=0; i<mails.length; i++) {
 
     // SE ELEMENTO DEL CICLO è UGUALE ALLA MAIL INSERITA
     if(mails[i] == new_mail) {
-        console.log('mail convalidata')
+        flag = true;
     }
-     // ALTRIMENTI
-    else {
-        console.log('mail non convalidata')
-    }
+     
+}
+
+if (flag == true) {
+    console.log('mail convalidata')
+}
+else {
+    console.log('mail non convalidata')
 }
